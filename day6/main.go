ab7f9b0382f891633824497dc20920e76de7c9e0
Abdallah Hodieb <abdallah.hodieb@gmail.com>
2023-02-20 11:34:51 +0000

    ⭐️⭐️ solve day 6

---
+++ day6/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ahodieb/advent-of-code-2022/common/input"
+)
+
+func main() {
+	in, err := input.FromPath("day6/input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer in.Close()
+
+	for in.Scan() {
+		fmt.Println(
+			findMarker(in.Text(), 4),
+			findMarker(in.Text(), 14),
+		)
+	}
+}
+
+func findMarker(signal string, size int) int {
+	for i := size; i < len(signal); i++ {
+		if allDiff(signal[i-size : i]) {
+			return i
+		}
+	}
+
+	return 0
+}
+
+func allDiff(marker string) bool {
+	hash := make(map[uint8]struct{})
+
+	for i := 0; i < len(marker); i++ {
+		if _, ok := hash[marker[i]]; ok {
+			return false
+		}
+
+		hash[marker[i]] = struct{}{}
+	}
+
+	return true
+}
