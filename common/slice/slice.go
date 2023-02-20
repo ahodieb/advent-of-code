217973d6999fa25ab8c8f0853c38fb83d3f51247
Abdallah Hodieb <abdallah.hodieb@gmail.com>
2023-02-20 11:34:52 +0000

    🛠 add `slice.Min` and `slice.Filter`

--- common/slice/slice.go
+++ common/slice/slice.go
@@ -8,3 +8,40 @@ func Sum(numbers ...int) int {
 
 	return s
 }
+
+func Min[T uint8 | int | int64 | float32 | float64](numbers ...T) T {
+	var min T
+	if len(numbers) == 0 {
+		return min
+	}
+
+	min = numbers[0]
+	for i := range numbers {
+		if numbers[i] < min {
+			min = numbers[i]
+		}
+	}
+
+	return min
+}
+
+func Filter[T any](items []T, filters ...func(i T) bool) []T {
+	var newSlice []T
+
+	for i := range items {
+		item := items[i]
+		included := true
+		for _, f := range filters {
+			if !f(item) {
+				included = false
+				break
+			}
+		}
+
+		if included {
+			newSlice = append(newSlice, item)
+		}
+	}
+
+	return newSlice
+}
