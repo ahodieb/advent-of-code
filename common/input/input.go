edd1007717e65921f99ed51b95c59ceea61630d1
Abdallah Hodieb <abdallah.hodieb@gmail.com>
2023-02-20 11:34:51 +0000

    ⭐️⭐️ solve day 3

--- common/input/input.go
+++ common/input/input.go
@@ -6,6 +6,15 @@ import (
 	"strconv"
 )
 
+func ChunkedFromPath(path string, chunk int) (*Chunked, error) {
+	in, err := FromPath(path)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Chunked{input: in, chunkSize: chunk}, nil
+}
+
 func FromPath(path string) (*Input, error) {
 	f, err := os.Open(path)
 	if err != nil {
@@ -29,6 +38,15 @@ func (i *Input) Scan() bool {
 	return i.scanner.Scan()
 }
 
+func (i *Input) ScanAll() []string {
+	var lines []string
+	for i.Scan() {
+		lines = append(lines, i.Text())
+	}
+
+	return lines
+}
+
 func (i *Input) Text() string {
 	return i.scanner.Text()
 }
@@ -36,4 +54,36 @@ func (i *Input) Text() string {
 func (i *Input) Int() int {
 	n, _ := strconv.Atoi(i.scanner.Text())
 	return n
+}
+
+type Chunked struct {
+	input     *Input
+	chunkSize int
+	chunk     []string
+}
+
+func (c *Chunked) Close()          { c.input.Close() }
+func (c *Chunked) Chunk() []string { return c.chunk }
+
+func (c *Chunked) Scan() bool {
+	c.chunk = nil
+
+	if c.chunkSize < 2 {
+		next := c.input.Scan()
+		if next {
+			c.chunk = []string{c.input.Text()}
+		}
+		return next
+	}
+
+	for i := 0; i < c.chunkSize; i++ {
+		next := c.input.Scan()
+		if !next {
+			return len(c.chunk) > 0
+		}
+
+		c.chunk = append(c.chunk, c.input.Text())
+	}
+
+	return true
 }
---
+++ day3/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ahodieb/advent-of-code-2022/common/input"
+)
+
+func main() {
+	part1()
+	part2()
+}
+
+func part2() {
+	in, err := input.ChunkedFromPath("day3/input.txt", 3)
+	if err != nil {
+		panic(err)
+	}
+	defer in.Close()
+
+	sum := 0
+	for in.Scan() {
+		item := findCommon(in.Chunk()...)
+		priority := findPriority(item)
+		sum += priority
+	}
+
+	fmt.Println(sum)
+}
+
+func part1() {
+	in, err := input.FromPath("day3/input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer in.Close()
+
+	sum := 0
+	for in.Scan() {
+		item := findItem(in.Text())
+		priority := findPriority(item)
+		sum += priority
+	}
+
+	fmt.Println(sum)
+}
+
+func findPriority(item uint8) int {
+	if item >= 'a' && item <= 'z' {
+		return int(item - 'a' + 1)
+	}
+
+	if item >= 'A' && item <= 'Z' {
+		return int(item - 'A' + 27)
+	}
+
+	return 0
+}
+
+func findItem(items string) uint8 {
+	return findCommon(items[:len(items)/2], items[len(items)/2:])
+}
+
+func findCommon(items ...string) uint8 {
+	hash := make(map[uint8]struct{})
+
+	for i := 0; i < len(items[0]); i++ {
+		hash[items[0][i]] = struct{}{}
+	}
+
+	for _, bag := range items {
+		intersection := make(map[uint8]struct{})
+
+		for i := 0; i < len(bag); i++ {
+			if _, ok := hash[bag[i]]; ok {
+				intersection[bag[i]] = struct{}{}
+			}
+		}
+		hash = intersection
+	}
+
+	for k := range hash {
+		return k
+	}
+
+	return 0
+}
