e76ed1ee7cff299001c35daa55772e17c7709e8b
Abdallah Hodieb <abdallah.hodieb@gmail.com>
2023-12-18 04:43:56 +0000

    ✨💩 solve day7 part2
    brute-forcing this one was impossible, it would have needed 21165830176709 steps.
    
    Reddit lead me to see that I need to LCM (the least common multiple) of all individual paths.
    
    calculating individual paths was trivial, and i skipped implementing LCM since i really don't understand the GCD implementation, and it's already late at night 😅

--- 2023/day8/main.go
+++ 2023/day8/main.go
@@ -17,24 +17,29 @@ func main() {
 	in.ReadLine()
 
 	m := make(map[string][]string)
+	var current []string
 	for in.Scan() {
 		lr := strings.Split(in.Text()[7:len(in.Text())-1], ",")
-		m[in.Text()[:3]] = []string{lr[0], lr[1][1:]}
-	}
-
-	c := "AAA"
-	d := 0
-	s := 0
-	for c != "ZZZ" {
-		n := 0
-		if directions[d] == 'R' {
-			n = 1
+		s := in.Text()[:3]
+		m[s] = []string{lr[0], lr[1][1:]}
+		if strings.HasSuffix(s, "A") {
+			current = append(current, s)
 		}
-
-		c = m[c][n]
-		d = (d + 1) % len(directions)
-		s += 1
 	}
 
-	fmt.Println(s)
+	steps := make([]int, len(current))
+	for i := range current {
+		d := 0
+		for !strings.HasSuffix(current[i], "Z") {
+			n := 0
+			if directions[d] == 'R' {
+				n = 1
+			}
+			current[i] = m[current[i]][n]
+			d = (d + 1) % len(directions)
+			steps[i] += 1
+		}
+	}
+
+	fmt.Println(steps) // LCM
 }
